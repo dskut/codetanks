97067e1af0a85c5fd0f291cd7525086b4d415366
Denis Kutukov <mail@dskut.ru>
2012-11-05 23:15:00 +0400

    go to corner in the beginning

--- src/main/java/MyStrategy.java
+++ src/main/java/MyStrategy.java
@@ -1,11 +1,37 @@
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 import static java.lang.StrictMath.PI;
 import model.*;
 
-
-enum State {
+class Point {
+	public double x;
+	public double y;
 	
+	public Point(double x, double y) {
+		this.x = x;
+		this.y = y;
+	}
+}
+
+class TankDistComparator implements Comparator<Point> {
+	private Tank tank;
+	public TankDistComparator(Tank tank) {
+		this.tank = tank;
+	}
+	public int compare(Point first, Point second) {
+		double diff = tank.getDistanceTo(first.x, first.y) - tank.getDistanceTo(second.x, second.y);
+		return (int)diff;
+	}
+}
+
+enum States {
+	Init, // in the start phase - sit in corner
+	Walk, // then - walk around map
 }
 
 public final class MyStrategy implements Strategy {
@@ -17,6 +43,24 @@ public final class MyStrategy implements Strategy {
 	private static double MIN_DURABILITY = 0.3;
 	private static double STABLE_DURABILITY = 0.6;
 	private static double MAX_PREMIUM_SHOOT_DIST = 500;
+	private static double XMIN = 100;
+	private static double YMIN = 100;
+	
+	private States state;
+	
+	public MyStrategy() {
+		state = States.Init;
+	}
+	
+	private void driveForward(Move move) {
+		move.setLeftTrackPower(1);
+		move.setRightTrackPower(1);
+	}
+	
+	private void driveBackward(Move move) {
+		move.setLeftTrackPower(-1);
+		move.setRightTrackPower(-1);
+	}
 	
 	private int getSmallestAngleEnemy(Tank self, World world) {
 		int index = -1;
@@ -108,7 +152,14 @@ public final class MyStrategy implements Strategy {
     	return getNearestBonus(self, world);
 	}
 	
+	private void drive(Tank self, Move move, Point point) {
+		drive(self, move, point.x, point.y);
+	}
+	
 	private void drive(Tank self, Move move, double x, double y) {
+		if (self.getDistanceTo(x, y) < self.getHeight()) {
+			return;
+		}
 		double angle = self.getAngleTo(x, y);    		
 		if (-PI/2 <= angle && angle <= PI/2) {
 			if (angle > MIN_DRIVE_ANGLE) {
@@ -118,8 +169,7 @@ public final class MyStrategy implements Strategy {
 				move.setLeftTrackPower(-1);
 				move.setRightTrackPower(0.75);
 			} else {
-				move.setLeftTrackPower(1);
-				move.setRightTrackPower(1);
+				driveForward(move);
 			}
 		} else {
 			if (0 > angle && angle > -PI + MIN_DRIVE_ANGLE) {
@@ -129,8 +179,7 @@ public final class MyStrategy implements Strategy {
 				move.setLeftTrackPower(-1);
 				move.setRightTrackPower(0.75);
 			} else {
-				move.setLeftTrackPower(-1);
-				move.setRightTrackPower(-1);
+				driveBackward(move);
 			}
 		}	}
 	
@@ -185,9 +234,72 @@ public final class MyStrategy implements Strategy {
 		return directEnemies >= enemies.size() - directEnemies;
 	}
 	
-    @Override
-    public void move(Tank self, World world, Move move) {
-    	Tank[] tanks = world.getTanks();
+	private boolean isEnemyCloser(Tank self, World world, Point point) {
+		double selfDist = self.getDistanceTo(point.x, point.y);
+		for (Tank tank: world.getTanks()) {
+			if (tank.getCrewHealth() > 0 && tank.getDistanceTo(point.x, point.y) < selfDist) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	private Point getNearestFreeCorner(Tank self, World world) {
+		Point[] corners = new Point[] {new Point(XMIN, YMIN), 
+									   new Point(XMIN, world.getHeight() - YMIN), 
+									   new Point(world.getWidth() - XMIN, YMIN), 
+									   new Point(world.getWidth() - XMIN, world.getHeight() - YMIN)};
+		Arrays.sort(corners, new TankDistComparator(self));
+		for (Point corner: corners) {
+			if (!isEnemyCloser(self, world, corner)) {
+				return corner;
+			}
+		}
+		return null;
+	}
+	
+	private Point getNearestWall(Tank self, World world) {
+		List<Point> walls = new ArrayList<Point>();
+		walls.add(new Point(XMIN, world.getHeight() / 2));
+		walls.add(new Point(world.getWidth() - XMIN, world.getHeight() / 2));
+		walls.add(new Point(world.getWidth() / 2, YMIN));
+		walls.add(new Point(world.getWidth() / 2, world.getHeight() - YMIN));
+		
+		Collections.sort(walls, new TankDistComparator(self));
+		for (Point wall: walls) {
+			if (!isEnemyCloser(self, world, wall)) {
+				return wall;
+			}
+		}
+		return walls.get(0);
+	}
+	
+	private List<Tank> getAliveTanks(World world) {
+		List<Tank> res = new ArrayList<Tank>();
+		for (Tank tank: world.getTanks()) {
+			if (tank.getCrewHealth() > 0) {
+				res.add(tank);
+			}
+		}
+		return res;
+	}
+	
+	private boolean shouldChangeState(Tank self, World world) {
+		if (self.getCrewHealth() < self.getCrewMaxHealth() * 0.66) {
+			return true;
+		}
+		if (self.getHullDurability() < self.getHullMaxDurability() * 0.66) {
+			return true;
+		}
+		List<Tank> alive = getAliveTanks(world);
+		if (alive.size() <= 4) {
+			return true;
+		}
+		return false;
+	}
+	
+	private void selectShootMove(Tank self, World world, Move move) {
+		Tank[] tanks = world.getTanks();
     	int smallestAngleEnemy = getSmallestAngleEnemy(self, world);
     	int nearestEnemy = getNearestEnemy(self, world);
     	
@@ -223,25 +335,45 @@ public final class MyStrategy implements Strategy {
     			}
     		}
     	}
-    	
-    	int bonusIndex = getImportantBonus(self, world);
-    	ArrayList<Tank> shootingEnemies = getShootingEnemies(self, world);
-    	if (!shootingEnemies.isEmpty()) {
-    		if (shouldHideForward(self, world, shootingEnemies)) {
-				move.setLeftTrackPower(1);
-				move.setRightTrackPower(1);
-    		} else {
-				move.setLeftTrackPower(-1);
-				move.setRightTrackPower(-1);
-    		}
-    	} if (bonusIndex != -1) {
-    		Bonus bonus = world.getBonuses()[bonusIndex];
-    		drive(self, move, bonus.getX(), bonus.getY());
-    	} else {
-    		double randX = Math.random() * world.getWidth();
-    		double randY = Math.random() * world.getHeight();
-    		drive(self, move, randX, randY);
-    	}
+	}
+	
+	private void selectDriveMove(Tank self, World world, Move move) {
+		if (state == States.Init) {
+			Point corner = getNearestFreeCorner(self, world);
+			if (corner == null) {
+				Point wall = getNearestWall(self, world);
+				drive(self, move, wall);
+			} else {
+				drive(self, move, corner);
+			}
+			if (shouldChangeState(self, world)) {
+				state = States.Walk;
+			}
+			return;
+		} 
+		if (state == States.Walk) {
+	    	int bonusIndex = getImportantBonus(self, world);
+	    	ArrayList<Tank> shootingEnemies = getShootingEnemies(self, world);
+	    	if (!shootingEnemies.isEmpty()) {
+	    		if (shouldHideForward(self, world, shootingEnemies)) {
+	    			driveForward(move);
+	    		} else {
+	    			driveBackward(move);
+	    		}
+	    	} if (bonusIndex != -1) {
+	    		Bonus bonus = world.getBonuses()[bonusIndex];
+	    		drive(self, move, bonus.getX(), bonus.getY());
+	    	} else {
+	    		double randX = Math.random() * world.getWidth();
+	    		double randY = Math.random() * world.getHeight();
+	    		drive(self, move, randX, randY);
+	    	}
+		}	}
+	
+    @Override
+    public void move(Tank self, World world, Move move) {
+    	selectShootMove(self, world, move);
+    	selectDriveMove(self, world, move);
     }
 
     @Override
